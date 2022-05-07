24f745dec2647e2dc2eab3f7c376d0bf91b9b6a0
Omer Faruk Dogan <omer.crlys.44@gmail.com>
2022-05-07 22:55:05 +0300

    Second commit

--- bookstore/books/models.py
+++ bookstore/books/models.py
@@ -1,3 +1,15 @@
+from unicodedata import name
 from django.db import models
-
-# Create your models here.
+class Author(models.Model):
+    def __str__(self):
+        return self.name
+    name=models.CharField(max_length=200,blank=False,null=False)
+    created=models.DateTimeField('Oluşturuldu.')
+class Book(models.Model):
+    def __str__(self):
+        return self.name
+    
+    name=models.CharField(max_length=200,blank=False,null=False)
+    created=models.DateTimeField('Oluşturuldu.')
+    author=models.ForeignKey(Author,on_delete=models.CASCADE)
+    price=models.DecimalField(decimal_places=2,max_digits=4,null=True)
--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Anasayfa')
+def authors(request):
+    return HttpResponse('Yazarlar')
+def books(request):
+    return HttpResponse('kitaplar')
+def authorDetails(request,authorId):
+    return HttpResponse('Yazar detayı :'+str(authorId))
